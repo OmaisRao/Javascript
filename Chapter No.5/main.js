9c1053f98ab3949392be2c8c5058b2261b106dbc
Omais Rao <154328445+OmaisRao@users.noreply.github.com>
2024-02-29 15:43:19 +0500

    add

---
+++ Chapter No.5/main.js
@@ -0,0 +1,129 @@
+console.log("Q1")
+//Q1
+var nuM1=5
+var nuM2=5
+console.log(nuM1+nuM2)
+//
+
+
+console.log("Q2")
+//Q2
+var nuM1=5
+var nuM2=5
+console.log(nuM1-nuM2)
+console.log(nuM1*nuM2)
+console.log(nuM1%nuM2)
+//
+
+
+console.log("Q3")
+//Q3
+var nUm=5+1+7-1
+console.log(nUm/3)
+//
+
+
+console.log("Q4")
+//Q4
+var tIcket=600
+var totaL=5
+console.log(tIcket*totaL)
+//
+
+console.log("Q5")
+//Q5
+console.log("Table of 4")
+console.log("4x1=4")
+console.log("4x2=8")
+console.log("4x3=12")
+console.log("4x4=16")
+console.log("4x5=20")
+console.log("4x6=24")
+console.log("4x7=28")
+console.log("4x8=32")
+console.log("4x9=36")
+console.log("4x10=40")
+//
+
+console.log("Q6")
+//Q6
+var c=25
+console.log(c*9/5+32+"F")
+var F=70
+F=70-32
+console.log(F*5/9 + "C")
+//
+
+console.log("Q7")
+//Q7
+console.log("Shopping Cart")
+
+console.log("Price of item.1 is 650$")
+console.log("Quantity of item.1 is 2")
+console.log("Price of item.2 is 500$")
+console.log("Quantity of item.2 is 1")
+console.log("Your Total bill is 1800$")
+//
+
+
+console.log("Q8")
+//Q8
+console.log("Marks Sheet")
+console.log("Total Marks=980")
+console.log("Marks Obtained=804")
+console.log("Percentage=82")
+//
+
+
+console.log("Q9")
+//Q9
+console.log("Currency in PKR")
+console.log("Total Currency in Dollar=8$")
+console.log("Total Currency in PKR=2,217.68Rs")
+//
+
+
+console.log("Q10")
+//Q10
+var nUm=10
+console.log(nUm+5*10/2)
+//
+
+
+
+console.log("Q11")
+//Q11
+console.log("Age Calculator")
+console.log("Current Year:2024")
+console.log("Birth Year:2006")
+console.log("Your Age Is:18")
+//
+
+
+console.log("Q12")
+//Q12
+var Heading="The Geometrizer"
+var Radius="The Radius of a circle is:20"
+var ciR="the circum ferenec is = 125"
+var totalArea=20*20*3.142
+console.log(Heading)
+console.log(Radius)
+console.log(ciR)
+console.log("The area is:"+totalArea)
+//
+
+
+console.log("Q13")
+//Q13
+var SnaCk="Choclate Chip"
+var aGe=18
+var mAxaGe=65
+var eatPerday=3
+var ToTal=47*3
+var eNd="to last You until the ripe old Age"
+console.log("Favourite Snack : "+SnaCk)
+console.log("Current Age: "+aGe)
+console.log("Estimated Max Age:"+mAxaGe)
+console.log("Amount of Snack Per Day:"+eatPerday)
+console.log("You will Need "+ToTal +SnaCk +eNd)
+//
---
+++ Chapter No.6/main.js
@@ -0,0 +1,46 @@
+console.log("Q1")
+//Q1
+var a = 10;
+var newNum = num++
+
+console.log('num->',num)
+console.log(newNum)
+
+
+//
+
+
+// console.log("Q1")
+// //Q1
+// var a=10
+// //
+
+
+// console.log("Q1")
+// //Q1
+// var a=10
+// //
+
+
+// console.log("Q1")
+// //Q1
+// var a=10
+// //
+
+
+// console.log("Q1")
+// //Q1
+// var a=10
+// //
+
+
+// console.log("Q1")
+// //Q1
+// var a=10
+// //
+
+
+// console.log("Q1")
+// //Q1
+// var a=10
+// //
